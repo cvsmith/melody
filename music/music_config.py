94006b8d6d6b8aa1af4f342e31e4ad7cdee871dd
cvsmith <cvsmith@andrew.cmu.edu>
2015-09-05 17:46:19 -0400

    Generate random music to ensure all MIDI functionality working

---
+++ music/music_config.py
@@ -0,0 +1,25 @@
+from mingus.containers import *
+
+# NOT major pentatonic, F instead of E
+PENT_SCALE = {
+    0: "C",
+    1: "D",
+    2: "F",
+    3: "G",
+    4: "A"
+}
+
+ALL_NOTES = []
+for octave in (3, 4, 5, 6):
+    for degree in xrange(5):
+        ALL_NOTES += [Note(PENT_SCALE[degree], octave)]
+
+print ALL_NOTES
+
+COLOR_INSTR_NRS = {
+    'r': 0,     # Acoustic Grand Piano
+    'g': 40,    # Violin
+    'b': 71,    # Clarinet
+}
+
+METER = (48, 4) # Beats per bar, duration of beat
--- music/music_processing.py
+++ music/music_processing.py
@@ -2,8 +2,10 @@ import miditoaudio
 from mingus.containers import *
 from mingus.containers.instrument import MidiInstrument
 from mingus.midi.midi_file_out import write_Composition
+from music_config import * 
+import random
 
-test_data = [
+TEST_DATA = [
     (0.0, 
         {
             'r': [0.4, 0.1],
@@ -27,54 +29,34 @@ test_data = [
     )
 ]
 
+# Write MIDI file corresponding to music data, return path to 
+def write_midi_file(data, filename):
+    comp = make_composition(data)
+
+# Return mingus composition object from data
+def make_composition(data):
+    comp = Composition()
+    for moment in data:
+        (time, tones) = moment
+
 
 comp = Composition()
 t1 = Track(Instrument())
-t1.instrument = MidiInstrument(name='Acoustic Grand Piano')
-t1.instrument.instrument_nr = 0
+t1.instrument.instrument_nr = COLOR_INSTR_NRS['r']
 t2 = Track(Instrument())
-t2.instrument = MidiInstrument(name='Violin')
-t2.instrument.instrument_nr = 40
+t2.instrument.instrument_nr = COLOR_INSTR_NRS['g']
 t3 = Track(Instrument())
-t3.instrument = MidiInstrument(name='Clarinet')
-t3.instrument.instrument_nr = 71
+t3.instrument.instrument_nr = COLOR_INSTR_NRS['b']
 
-
-tracks = [t1, t2, t3]
-
-notes = NoteContainer(['C-4', 'E-4', 'G-4', 'C-5'])
-
-bar1 = Bar(key='C', meter=(4, 4))
-bar1.place_notes(notes[0], 4)
-bar1.place_rest(4)
-bar1.place_rest(4)
-bar1.place_notes(notes[2], 4)
-t1.add_bar(bar1)
-
-bar2 = Bar(key='C', meter=(4, 4))
-if len(bar2) == 0:
-    bar2.place_rest(12)
-else:
-    bar2.place_rest(4)
-bar2.place_notes(notes[1], 4)
-if len(bar2) == 0:
-    bar2.place_rest(12)
-else:
-    bar2.place_rest(4)
-bar2.place_notes(notes[1], 4)
-t2.add_bar(bar2)
-
-bar3 = Bar(key='C', meter=(4, 4))
-bar3.place_rest(12)
-bar3.place_rest(12)
-bar3.place_notes(notes[2], 4)
-bar3.place_notes(notes[3], 4)
-t3.add_bar(bar3)
-
-for t in tracks:
-    comp.add_track(t)
+for track in [t1, t2, t3]:
+    bar = Bar(key='C', meter=METER)
+    for i in xrange(int(bar.length) * 4):
+        note = random.choice(ALL_NOTES+[None])
+        bar.place_notes(note, METER[1])
+    track.add_bar(bar)
+    comp.add_track(track)
 
 print comp
 
-write_Composition('test_composition.mid', comp)
+write_Composition('test_composition.mid', comp, 180)
     
