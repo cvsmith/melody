f9227173d8a8fd2d6f920d3d12f33786dd3e8822
cvsmith <cvsmith@andrew.cmu.edu>
2015-09-05 18:28:16 -0400

    Add wav file merging

--- music/music_processing.py
+++ music/music_processing.py
@@ -1,9 +1,10 @@
 import miditoaudio
 from mingus.containers import *
 from mingus.containers.instrument import MidiInstrument
-from mingus.midi.midi_file_out import write_Composition
+from mingus.midi.midi_file_out import write_Composition, write_Track
 from music_config import * 
 import random
+import wave
 
 TEST_DATA = [
     (0.0, 
@@ -32,6 +33,8 @@ TEST_DATA = [
 # Write MIDI file corresponding to music data, return path to 
 def write_midi_file(data, filename):
     comp = make_composition(data)
+    write_Composition(filename, comp, 180)
+
 
 # Return mingus composition object from data
 def make_composition(data):
@@ -39,24 +42,37 @@ def make_composition(data):
     for moment in data:
         (time, tones) = moment
 
+    comp = Composition()
+    t1 = Track(Instrument())
+    t1.instrument.instrument_nr = COLOR_INSTR_NRS['r']
+    t2 = Track(Instrument())
+    t2.instrument.instrument_nr = COLOR_INSTR_NRS['g']
+    t3 = Track(Instrument())
+    t3.instrument.instrument_nr = COLOR_INSTR_NRS['b']
 
-comp = Composition()
-t1 = Track(Instrument())
-t1.instrument.instrument_nr = COLOR_INSTR_NRS['r']
-t2 = Track(Instrument())
-t2.instrument.instrument_nr = COLOR_INSTR_NRS['g']
-t3 = Track(Instrument())
-t3.instrument.instrument_nr = COLOR_INSTR_NRS['b']
+    count = 0
+    for track in [t1, t2, t3]:
+        bar = Bar(key='C', meter=METER)
+        for i in xrange(int(bar.length) * 4):
+            note = random.choice(ALL_NOTES+[None])
+            bar.place_notes(note, METER[1])
+        track.add_bar(bar)
+        write_Track('track'+count+'mid', 180)
+        count += 1
+        comp.add_track(track)
 
-for track in [t1, t2, t3]:
-    bar = Bar(key='C', meter=METER)
-    for i in xrange(int(bar.length) * 4):
-        note = random.choice(ALL_NOTES+[None])
-        bar.place_notes(note, METER[1])
-    track.add_bar(bar)
-    comp.add_track(track)
 
-print comp
+# Merge wave files
+def merge_wave_files(infiles, outfile):
+    data= []
+    for infile in infiles:
+        w = wave.open(infile, 'rb')
+        data.append( [w.getparams(), w.readframes(w.getnframes())] )
+        w.close()
 
-write_Composition('test_composition.mid', comp, 180)
+    output = wave.open(outfile, 'wb')
+    output.setparams(data[0][0])
+    output.writeframes(data[0][1])
+    output.writeframes(data[1][1])
+    output.close()
     
