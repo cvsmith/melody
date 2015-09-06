70eb8a7319fd0ef2fbc3f7e67c1d50f1396198c2
Ubuntu <hackers@composition-vm.fesoqdlw3r5eph1ktf5hsa5uob.bx.internal.cloudapp.net>
2015-09-06 11:22:14 +0000

    naming files (I just woke up)

--- server.py
+++ server.py
@@ -15,27 +15,35 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 melody = Flask(__name__)
 melody.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@melody.route("/", methods=['POST'])
+@melody.route("/", methods=['GET','POST'])
 def app():
     try:
         if request.method == 'POST':
             print dir(request)
             print 'data:{}'.format(request.data)
-            image_path = save_image(request)
+            name = random_name()
+            image_path = save_image(name,request)
             image_data = do_cv(image_path)
-            wav_path   = make_music(image_data)
+            wav_path   = make_music(name,image_data)
             wav_path = url_for('static', filename='output.wav')
             return redirect(wav_path)
+        else:
+            return melody.send_static_file('index.html')
     except:
         import traceback,sys
         print (sys.exc_info()[0])
         traceback.print_tb(sys.exc_info()[2])
 
+def random_name():
+    return ''.join(random.choice(string.ascii_uppercase +
+                                 string.ascii_lowercase)
+                   for _ in range(6))
+
 # Save image to images folder and return path to that folder
 def save_image(request):
     if request.method == 'POST':
         file = request.files['file']
-        name = str(datetime.now()) + '.' + file.filename
+        name = name + '.' + str(datetime.now()) + '.' + file.filename
         if file and allowed_file(name):
             filename = secure_filename(name)
             path = os.path.join(melody.config['UPLOAD_FOLDER'], filename)
@@ -46,15 +54,16 @@ def do_cv(imgPath):
     img = cv2.imread(imgPath)
     return cv.processImage(img)[0]
 
-def make_music(imgData):
+def make_music(name,imgData):
     print imgData
     currDir = os.getcwd()
     os.chdir("./music")
     # imgData = json.loads(json.dumps(imgData))
-    mp.main(imgData)
-    call(["mv", "./output.wav", "../static/"])
+    outname = name + '.wav'
+    mp.main(imgData,outfile=outname)
+    call(["mv", os.joinpath('.',outname), "../static/"])
     os.chdir(currDir)
-    return "./static/output.wav"
+    return os.joinpath('.','static',outname)
 
 # Return True if filename is allowed
 def allowed_file(filename):
